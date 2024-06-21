2f27a28a2731117a1e603287fb2c4edff308a006
Mrunal Tarwatkar <mrunatarwatkar018@gmail.com>
2024-06-21 20:31:33 +0530

    First Commit

---
+++ 04_counterproject_usingHooks/src/App.jsx
@@ -0,0 +1,111 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  const [counter, setCounter] = useState(0);
+
+
+  // let counter = 15;
+
+  const addValue = () => {
+    // counter = counter + 1;
+    setCounter(prevCounter => prevCounter + 1);
+  }
+
+  const removeValue = () => {
+    if (counter > 0)
+      setCounter(counter - 1);
+  }
+
+
+  return (
+    <>
+      <h1> Mrunals Counter</h1>
+      
+      <h2>Counter Value: {counter} </h2>
+
+      <button
+        onClick={addValue}
+      >Increment Value {counter} </button>
+
+      <br></br>
+      <br></br>
+
+      <button
+        onClick={removeValue}
+        disabled={counter === 0}
+      >Decrement Value {counter} </button>
+
+      <p>footer: {counter}</p>
+    </>
+  )
+}
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { useState } from 'react'
+// import './App.css'
+
+// function App() {
+
+//   const [counter, setCounter] = useState(15);
+
+
+//   // let counter = 15;
+
+//   const addValue = () => {
+//     console.log("clicked", counter);
+//     // counter = counter + 1;
+//     setCounter(counter + 1)
+//   }
+
+//   const removeValue = () => {
+//     console.log("Value Removed", counter);
+//     // counter = counter - 1;
+//     setCounter(counter - 1);
+//   }
+
+
+//   return (
+//     <>
+//       <h1> Mrunals Counter</h1>
+//       <h2>Counter Value: {counter} </h2>
+//       <button
+//         onClick={addValue}
+//       >Add Value {counter} </button>
+//       <br></br>
+//       <br></br>
+//       <button
+//         onClick={removeValue}
+//       >Remove Value {counter} </button>
+
+//       <p>footer: {counter}</p>
+//     </>
+//   )
+// }
+
+// export default App
