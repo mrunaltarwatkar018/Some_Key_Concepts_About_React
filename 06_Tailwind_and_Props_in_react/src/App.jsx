5010cd98cb866302039893253ae12eb89bd4ab41
Mrunal Tarwatkar <mrunatarwatkar018@gmail.com>
2024-07-02 00:48:05 +0530

    06_Tailwind_and_Props_in_react

---
+++ 06_Tailwind_and_Props_in_react/src/App.jsx
@@ -0,0 +1,88 @@
+// import { useState } from 'react'
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+
+  // let myObj = {
+  //   username: "hitesh",
+  //   age: 21
+  // };
+
+  // let newArr = [1, 2, 3]
+
+  return (
+    <>
+      <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>Tailwind test</h1>
+      <Card username="chaiaurcode" btnText="click me" />
+      <Card username="mrunal" />
+    </>
+  )
+}
+
+export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* Tailwind css */
+
+// import './App.css'
+
+// function App() {
+
+//   return (
+//     <>
+//       <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4' >Tailwind Test</h1>
+
+//       <figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
+//   <img className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="https://gratisography.com/wp-content/uploads/2023/10/gratisography-pumpkin-scarecrow-1170x780.jpg" alt="" width="384" height="512" />
+//   <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
+//     <blockquote>
+//       <p className="text-lg font-medium">
+//         “Tailwind CSS is the only framework that I &apos ve seen scale
+//         on large teams. It’s easy to customize, adapts to any design,
+//         and the build size is tiny.”
+//       </p>
+//     </blockquote>
+//     <figcaption className="font-medium">
+//       <div className="text-sky-500 dark:text-sky-400">
+//         Sarah Dayan
+//       </div>
+//       <div className="text-slate-700 dark:text-slate-500">
+//         Staff Engineer, Algolia
+//       </div>
+//     </figcaption>
+//   </div>
+// </figure>
+
+//     </>
+//   )
+// }
+
+// export default App
+
+/*
+    Links:
+
+    https://tailwindcss.com/
+
+
+    https://www.devui.io/components/cards
+
+*/
